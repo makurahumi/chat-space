cb4e90d2e51ec0877088c8b2f768d059580664dc
makurahumi <ethelandce.01kannna@gmail.com>
2019-10-19 20:39:21 +0900

    add_edit_controller_new

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
-  has_many :users, through: :user_groups
-  has_many :user_groups
+  has_many :users, through: :group_users
+  has_many :group_users
   has_many :comments
 
   validates :name, presence: true, uniqueness: true
