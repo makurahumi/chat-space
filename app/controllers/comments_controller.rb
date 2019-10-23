7f913ce5d88361a97195a5c7bef0832bf7d0d1dd
makurahumi <ethelandce.01kannna@gmail.com>
2019-10-23 17:14:12 +0900

    add_edit_comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,28 @@
 class CommentsController < ApplicationController
+  before_action :set_group
+
   def inedx
+    @comment = Comment.new
+    @comments = @group.comments.includes(:user)
   end
 
   def create
+    @comment = @group.comments.new(comments_params)
+    if @comment.save
+      redirect_to group_comments_path(@group), notice: "コメントが送信されました"
+    else
+      @comments = @group.comments.includes(:user)
+      flash.now[:alert] = "コメントを入力してください"
+      render :index
+    end
   end
+
+  private
+    def comments_params
+      params.require(:comment).permit(:comment, :image).merge(user_id: current_user.id)
+    end
+
+    def set_group
+      @group = Group.find(params[:group_id])
+    end
 end
