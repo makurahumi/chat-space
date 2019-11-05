c403c077f101785cb267eca4088e93378e83566a
makurahumi <ethelandce.01kannna@gmail.com>
2019-11-05 20:49:55 +0900

    add_incremental_search

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,17 +2,17 @@ $(function() {
   
   var search_list = $("#user-search-result")
   
-  function appendUser(user) {
+  function addUser(user) {
     let html = `
               <div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name"${user.name}">追加</div>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
               </div>
               `;
     search_list.append(html);
   }
 
-  function appendNoUser(){
+  function addNoUser(){
     let html = `
               <div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">ユーザーが見つかりません</p>
@@ -21,10 +21,15 @@ $(function() {
     search_list.append(html);
   }
 
-  function addDeleteUser(name, id){
+  function addDeleteUser(name, id) {
     let html = `
-              <div class="chat-group-user">
-                <input name="group[user_ids][]" type="hidden value="#{user.id}">`
+              <div class="chat-group-user clearfix">
+              <input name="group[user_ids][]" type="hidden" value="${id}" />
+                <p class="chat-group-user__name">${name}</p>
+                <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+              </div>
+              `;
+    $(".js-add-user").append(html);
   }
 
   $("#user-search-field").on("keyup",function() {
@@ -40,19 +45,31 @@ $(function() {
 
       if (users.length !== 0) {
         users.forEach(function(user) {
-          appendUser(user);
+          addUser(user);
         });
-      }
-      else{
-        appendNoUser();
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
       }
     })
     .fail(function() {
       alert("通信エラーです。ユーザーが表示できません");
     });
   });
+  $(document).on('click', ".chat-group-user__btn--add", function(){
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName, userId);
+  });
+  $(document).on('click', ".chat-group-user__btn--remove", function(){
+    $(this)
+      .parent()
+      .remove();
+  });
 });
 
-$(document).on('click', ".chat-group-user__btn--add", function(){
 
-});
