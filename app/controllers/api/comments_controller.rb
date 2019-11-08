eaba2561db6c0e0589d28126aeb8fbde91f58b6c
makurahumi <ethelandce.01kannna@gmail.com>
2019-11-08 18:40:55 +0900

    edit_5fails

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -2,7 +2,8 @@ class Api::CommentsController < ApplicationController
   
   def index
     group = Group.find(params[:group_id])
-    last_comment_id = params[:id].to_i
+    last_comment_id = params[:last_id].to_i
     @comments = group.comments.includes(:user).where("id > #{last_comment_id}")
+    
   end
 end
